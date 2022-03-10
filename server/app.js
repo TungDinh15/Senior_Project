1a60759bba478cf4c5b4fb62f3fc74ce2e989738
TungDinh15 <dinhthanhtung150200@gmail.com>
2022-03-10 16:53:36 +0700

    Continue

--- server/app.js
+++ server/app.js
@@ -7,11 +7,113 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+// Configure ENV file & Require Connection File
+dotenv.config({ path: './config.env' });
+require('./db/conn');
+const port = process.env.PORT;
+
+// Require Model
+const Users = require('./models/userSchema.js');
+const Message = require('./models/msgSchema');
+
+// These Methods are used to get Data and Cookies from Frontend
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Render
 app.get('/', (req, res) => {
     res.send("Hello world")
 });
 
+// Registration
+app.post('/register', async (req, res) => {
+    try {
+        // Get body or Database
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const createUser = new Users({
+            username: username,
+            email: email,
+            password: password
+        });
+
+        // Save Method is Used to Create User or Insert User
+        const created = await createUser.save();
+        console.log(created);
+        res.status(200).send("Registered");
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// Login User
+app.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        // Find User if Exist
+        const user = await Users.findOne({ email: email });
+        if (user) {
+            // Verify Password
+            const isMatch = await bcryptjs.compare(password, user.password);
+
+            if (isMatch) {
+                // Generate Token Which is Define in User Schema
+                const token = await user.generateToken();
+                res.cookie("jwt", token, {
+                    // Expires Token in 24 Hours 
+                    expires: new Date(Date.now + 86400000),
+                    httpOnly: true
+                })
+                res.status(200).send("Logged In");
+            } else {
+                res.status(400).send("Invalid Credentials");
+            }
+        } else {
+            res.status(400).send("Invalid Credentials");
+        }
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// Message
+app.post('/message', async (req, res) => {
+    try {
+        // Get body or Database
+        const name = req.body.name;
+        const email = req.body.email;
+        const message = req.body.message;
+
+        const sendMsg = new Message({
+            name: name,
+            email: email,
+            message: message
+        });
+
+        // Save Method is Used to Create User or Insert User
+        const created = await sendMsg.save();
+        console.log(created);
+        res.status(200).send("Sent");
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+// Logout Page
+app.get('/logout', (req, res) => {
+    res.clearCookie("jwt", {path: '/'})
+    res.status(200).send("User Logged Out")
+})
+
 // SERVER RUN
-app.listen(3001, () => {
+app.listen(port, () => {
     console.log("Server is running")
 })
